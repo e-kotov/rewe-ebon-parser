a8694604b681cbb344ba0ee20cb66c36b5324c8b
Egor Kotov <kotov.egor@gmail.com>
2024-08-04 15:58:09 +0200

    add simple version printing

--- src/rewe_ebon_parser/__init__.py
+++ src/rewe_ebon_parser/__init__.py
@@ -1,1 +1,5 @@
-# __init__.py
+# src/rewe_ebon_parser/__init__.py
+
+__version__ = "0.0.4"
+
+from .parse import parse_ebon, parse_pdf_ebon
--- src/rewe_ebon_parser/cli.py
+++ src/rewe_ebon_parser/cli.py
@@ -1,21 +1,23 @@
-# cli.py
+# src/rewe_ebon_parser/cli.py
 import sys
 from pathlib import Path
-from .output import process_pdf, process_folder
 import argparse
+from .output import process_pdf, process_folder
+from . import __version__
 
 def main():
     parser = argparse.ArgumentParser(description="Parse REWE eBons from PDF to JSON.")
-    parser.add_argument("input_path", type=str, help="Path to input PDF file or folder containing PDF files.")
+    parser.add_argument("input_path", type=str, nargs='?', help="Path to input PDF file or folder containing PDF files.")
     parser.add_argument("output_path", type=str, nargs='?', default=None, help="Path to output JSON file or folder for JSON files.")
     parser.add_argument("--file", action="store_true", help="Specify if the input and output paths are files.")
     parser.add_argument("--folder", action="store_true", help="Specify if the input and output paths are folders.")
     parser.add_argument("--nthreads", type=int, default=None, help="Number of concurrent threads to use for processing files.")
     parser.add_argument("--rawtext-file", action="store_true", help="Output raw text extracted from the PDF files to .txt files.")
     parser.add_argument("--rawtext-stdout", action="store_true", help="Print raw text extracted from the PDF files to the console.")
+    parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}", help="Show the version number and exit.")
 
     args = parser.parse_args()
-    input_path = Path(args.input_path)
+    input_path = Path(args.input_path) if args.input_path else None
     output_path = Path(args.output_path) if args.output_path else None
     max_workers = args.nthreads
     rawtext_file = args.rawtext_file
@@ -39,24 +41,28 @@ def main():
             sys.exit(1)
     else:
         # Auto-detection mode
-        if input_path.is_dir():
-            if not output_path:
-                output_path = input_path / 'rewe_json_out'
-            if output_path.is_dir() or not output_path.exists():
-                process_folder(input_path, output_path, max_workers, rawtext_file, rawtext_stdout)
+        if input_path:
+            if input_path.is_dir():
+                if not output_path:
+                    output_path = input_path / 'rewe_json_out'
+                if output_path.is_dir() or not output_path.exists():
+                    process_folder(input_path, output_path, max_workers, rawtext_file, rawtext_stdout)
+                else:
+                    print("Error: Output path should be a directory when the input path is a directory.")
+                    sys.exit(1)
+            elif input_path.is_file():
+                if not output_path:
+                    output_path = input_path.with_suffix('.json')
+                if output_path.is_file() or not output_path.exists():
+                    process_pdf(input_path, output_path, rawtext_file, rawtext_stdout)
+                else:
+                    print("Error: Output path should be a file when the input path is a file.")
+                    sys.exit(1)
             else:
-                print("Error: Output path should be a directory when the input path is a directory.")
-                sys.exit(1)
-        elif input_path.is_file():
-            if not output_path:
-                output_path = input_path.with_suffix('.json')
-            if output_path.is_file() or not output_path.exists():
-                process_pdf(input_path, output_path, rawtext_file, rawtext_stdout)
-            else:
-                print("Error: Output path should be a file when the input path is a file.")
+                print("Error: Invalid input or output path.")
                 sys.exit(1)
         else:
-            print("Error: Invalid input or output path.")
+            print("Error: No input path provided.")
             sys.exit(1)
 
 if __name__ == '__main__':
